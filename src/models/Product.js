81f799935b571cbf7b8a9c996ae0aafc63091b5c
wandieinnocent <wandieinnocent2@gmail.com>
2025-05-23 18:19:45 +0300

    WIP, product schema

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    category: String,
-    img: String,
-    quantity: Number,
-    price: Number,
+    product_code: { type: String, required: true },
+    product_name: { type: String, required: true },
+    supplier_id: { type: Schema.Types.ObjectId, ref: 'Supplier', required: true },
+    brand_id: { type: Schema.Types.ObjectId, ref: 'Brand', required: true },
+    branch_id: { type: Schema.Types.ObjectId, ref: 'Branch', required: true },
+    parent_product_category_id: { type: Schema.Types.ObjectId, ref: 'parentProductCategory', required: true },
+    product_category_id: { type: Schema.Types.ObjectId, ref: 'ProductCategory', required: true },
+    unit_id: { type: Schema.Types.ObjectId, ref: 'Unit', required: true }, //handle its api first
+    product_created_date: { type: Date, default: Date.now },
+    product_expiry_date: { type: Date, default: null },
+    product_stock_quantity: { type: Number, default: null },
+    product_cost_price: { type: Number, default: null },
+    product_selling_price: { type: Number, default: null },
+    product_status: { type: Number, enum: [0, 1], default: 1 }, // 0 = Inactive, 1 = Active
+    product_description: { type: String, default: null },
+    product_stock_alert: { type: Number, default: null },
+    product_image: { type: Schema.Types.Mixed, default: null },
+    product_image_gallery: { type: Schema.Types.Mixed, default: null },
+    created_by: { type: Schema.Types.ObjectId, ref: 'User', default: null },
+    updated_by: { type: Schema.Types.ObjectId, ref: 'User', default: null },
 },
     {
         timestamps: true,
