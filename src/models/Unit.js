edf5b3f97cc59a7e78e63dd451b800043834c2f5
wandieinnocent <wandieinnocent2@gmail.com>
2025-05-23 21:32:17 +0300

    Add unit validation

---
+++ src/models/Unit.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const unitSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    status: {
+        type: Number,
+        enum: [0, 1], // 0 = Inactive, 1 = Active
+        default: 1
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    },
+    updated_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Unit', unitSchema);
